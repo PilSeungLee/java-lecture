30adfd8b31a4eba188de6b6cd19219f5d016fe5a
꼬님꼬님 <leeps7979@gmail.com>
2021-12-24 21:58:09 +0900

    구구단 숙제 등

---
+++ Day05/src/GuguExample.java
@@ -0,0 +1,22 @@
+
+public class GuguExample {
+
+	public static void main(String[] args) {
+		
+		//for (int x = 2; x <=9; x++) {
+		//for (int y = 1; y <=9; y++) {
+		//	System.out.println(x + "*" + y + "=" + (x*y));
+		}
+		
+		
+	//구구단 옆으로 출력 숙제, for 문을 while 문으로 바꿔보기 숙제
+
+	int x = 2, y = 1; {
+	while ( x++ < 10)
+	while ( y++ < 10) {
+		System.out.println(x + "*" + y + "=" + (x*y));
+    } 
+}
+		
+	
+}
