729c25383dfe4009d0197265860e9da4927c2c76
꼬님꼬님 <leeps7979@gmail.com>
2022-01-04 21:48:24 +0900

    오버라이딩 오버로딩

---
+++ Day08/src/Application5.java
@@ -0,0 +1,31 @@
+//오버로딩
+
+public class Application5 {
+
+	//public static void main(String[] args) {
+
+	
+}
+
+
+class Bus {
+	
+	int wheel = 4;
+	
+	void pushButton() {
+		System.out.println("문열림");
+	}
+	
+	void pushButton(int count) {
+	    if(count<=3) {
+	    	System.out.println("문열림");
+	    } else {
+	    	System.out.println("그만 눌러");
+	    }
+	}
+	
+	
+}
+	
+
+
